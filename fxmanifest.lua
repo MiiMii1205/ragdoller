a6dadeeead96e8f6994bcf8439134696abc6cafd
Jérémy Bolduc <BoldJe1.work@gmail.com>
2020-12-20 13:46:36 -0500

    Initial Commit

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+
+game  'gta5'
+
+author 'MiiMii1205'
+description 'A simple script for enabling/disabling ragdolls on any aimed ped'
+version '0.0.1'
+
+client_script 'Scripts/rag.lua'
+export 'CheckRagdoller'
