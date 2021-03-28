ed593e354c714e01214f449eefcb04577986e386
Jérémy Bolduc <BoldJe1.work@gmail.com>
2021-03-28 13:15:53 -0400

    Added a repository field in the fxmanifest.lua file

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,6 +5,7 @@ game 'gta5'
 author 'MiiMii1205'
 description 'A simple script for enabling/disabling ragdolls on any aimed ped'
 version '1.0.0'
+repository 'https://github.com/MiiMii1205/ragdoller'
 
 client_scripts { 'Scripts/rag.lua', 'Scripts/pedTag.lua' }
 export 'CheckRagdoller'
